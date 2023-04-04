ba702dc2a4e7d655ddfe4ae62b5e2a5f7203feaa
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-04 17:14:28 -0300

    Almost there

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,24 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store3 = Store.find(4)
+@store4 = Store.find(5)
+@store5 = Store.find(6)
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Ivan", last_name: "Chew", hourly_rate: 80)
+@store3.employees.create(first_name: "Catherine", last_name: "Gillis", hourly_rate: 100)
+@store4.employees.create(first_name: "Esha", last_name: "Gillis", hourly_rate: 60)
+@store5.employees.create(first_name: "Zeina", last_name: "Chew", hourly_rate: 60)
+
+employees = Employee.all
+
+puts employees.count
+
+employees.each do |employee|
+    puts "Name: #{employee.first_name} #{employee.last_name}"
+end
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,48 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+    validates :store, presence: true
+    validates :first_name, length: {minimum: 3}
+    validates :annual_revenue, presence: true
+    validate :must_carry_apparel
+
+    def must_carry_apparel
+        if !mens_apparel? && !womens_apparel?
+            errors.add(:mens_apparel, "must carry at least one line of apparel")
+    end
+  end
+end
+
+puts "Enter a store name:"
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+
+if store.save
+    employee = store.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+
+if store.errors.any?
+puts "Failed to save store:"
+store.errors.full_messages.each do |message|
+  puts "- #{message}"
+end
+
+else
+    puts "Employee saved successfully!"
+  end
+
+else
+  puts "Failed to save store:"
+  store.errors.full_messages.each do |message|
+    puts "- #{message}"
+
+    end
+  end
+  
+  
+
