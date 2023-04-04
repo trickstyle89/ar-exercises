b4bfedb5882d71f1f603569fb2aa6eeb33ea5402
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-04 16:19:30 -0300

    Update exercise_5.rb

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total annual Revenue for all stores #{total_revenue}"
+puts "Total Stores #{Store.count}"
+puts "Average revenue for all stores #{total_revenue / Store.count}"
+big_stores = Store.where("annual_revenue < ?", 1000000).count
+puts "Number of stores with revenues of more that 1M: #{big_stores}"
+
+
